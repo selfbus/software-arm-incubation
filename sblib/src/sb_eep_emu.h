41bdcd99ccaef8658bec6db2ed931a8041bbc9f0
Martin Glueck <martin@mangari.org>
2013-12-14 10:51:23 +0100

    License change to GPL version 3

--- sblib/src/sb_eep_emu.h
+++ sblib/src/sb_eep_emu.h
@@ -1,8 +1,8 @@
 /*
- *  Copyright (c) 2013 Martin Glück <martin@mangari.org>
+ *  Copyright (c) 2013 Martin Glueck <martin@mangari.org>
  *
  *  This program is free software; you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License version 2 as
+ *  it under the terms of the GNU General Public License version 3 as
  *  published by the Free Software Foundation.
  */
 
