a57935321daad5e5cb515af1da3e7be938ddefd9
uwe223 <uwe-s@t-online.de>
2020-03-08 19:03:22 +0100

    commented out #ifdef

--- actuators/led-controller/dim4-bim112/inc/config.h
+++ actuators/led-controller/dim4-bim112/inc/config.h
@@ -29,10 +29,10 @@ const HardwareVersion hardwareVersion[2] =
 { 4, 0x003D, 0x22, { 0, 0, 0, 0, 0x03, 0x3D }, "AKD-0424V.02 RGBW LED Controller" },
 };
 
-#ifdef LED4
+//#ifdef LED4
 #define HARDWARE_ID 1			//TODO test
 #define PWM
-#endif
+//#endif
 
 
 
