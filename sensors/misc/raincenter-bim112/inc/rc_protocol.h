4e730058b04e119f241faba9f76a3cbd9ffb744f
Darthyson <d.h.enders@gmx.de>
2020-09-22 21:37:45 +0200

    App Raincenter: more KNX-objects added for rcDisplayMessage added decoding for rcDisplayMessage according to documentation of PCA/TWN Software
    Raincenter objects for testing
    JAL-410.01 Kanal A Rolladen
    Object   Name                        Function
    13       Rolladen Auf/Ab             TapWaterRefill (on/off)
    15       Stop                        TapWaterExchangeActive (on/off)
    17       Status aktuelle Richtung    TapWaterRefill Status (on/off)
    20       Status aktuelle Position    Calibrated Fill Level in m3 * 10 (43 means 4.3m³)
    22       Akt. Position gueltig       Alarm (on/off)
    23       Referenzfahrt starten       Pump active (on/off)
    25       Status obere Position       AutomaticTapwaterRefill Status (on/off)
    26       Status untere Position      ManualTapwaterRefill Status (on/off)

--- sensors/misc/raincenter-bim112/inc/rc_protocol.h
+++ sensors/misc/raincenter-bim112/inc/rc_protocol.h
@@ -16,6 +16,7 @@ Baudrate settings: 2400 8N1 (2400 Baud, 8 Databits, No Parity, 1 Stopbit)
 so far known commands (single char):
 Command         Description
 p               Parameter request
+s               Parameter Set (raincenter acknowledges command with 's', then it waits for 11 configuration bytes
 w               Display data request
 c               Switch Display (m³, cm, percent)
 a               Switch to tap water refill
@@ -24,86 +25,58 @@ b               Switch to reservoir
 -------------------------------------------------------------------------------
 p Parameter request Answer:
 Example in hex
-Byte#:     01 02 03 04 05 06 07 08 09 10 11 12 13 14 15 16 17
+Byte#:     00 01 02 03 04 05 06 07 08 09 10 11 12 13 14 15 16
 Data :     70 30 07 01 03 01 42 00 31 01 30 06 51 51 97 00 00
 
+byte numbering according to PROFIRAIN PCA documentation
 #byte   Description                             Multiplier  Unit    Code
- 1.     0x70 = 'p'
- 2.     water exchange period                       1       days    BCD
- 3.     tap water switch on height                  5       cm      BCD
- 4.     tap water switch on hysteresis              2       cm      BCD
- 5.     water exchange duration                     1       min     BCD
- 6.     tap water supply type                       -       0-2     BCD
- 7.     maximum fill level                          5       cm      BCD
- 8.     reservoir type                              -       0-1     BCD
- 9.     reservoir area                              0.1     m2      BCD
-10.     optional relay function                    -       0-9     BCD
-11.     automatic timer interval                    1       days    BCD
-12.     automatic timer duration                    10      s       BCD
-13.     fill level factory calibration              1       -       BCD
-14.     fill level user calibration                 1       -       BCD
-15.     fill level measured (raw data)              1       cm      BCD
-16.     fill level measured (raw data)              100     cm      BCD
-17.     0x00 (end of message ?)
+ 0.     0x70 = 'p'
+ 1.     water exchange period                       1       days    BCD
+ 2.     tap water switch on height                  5       cm      BCD
+ 3.     tap water switch on hysteresis              2       cm      BCD
+ 4.     water exchange duration                     1       min     BCD
+ 5.     tap water supply type                       -       0-2     BCD
+ 6.     maximum fill level                          5       cm      BCD
+ 7.     reservoir type                              -       0-1     BCD
+ 8.     reservoir area                              0.1     m2      BCD
+ 9.     optional relay function                     -       0-9     BCD
+10.     automatic timer interval                    1       days    BCD
+11.     automatic timer duration                    10      s       BCD
+12.     fill level factory calibration              1       -       BCD
+13.     fill level user calibration                 1       -       BCD
+14.     fill level measured (raw data)              1       cm      BCD
+15.     fill level measured (raw data)              100     cm      BCD
+16.     0x00 (end of message ?)
 
 -------------------------------------------------------------------------------
 w Display data request Answer:
 Example in hex
-Byte#:   01 02 03 04 05
+Byte#:   00 01 02 03 04
 Data:    77 95 00 08 02
 
+byte numbering according to PROFIRAIN PCA documentation
 #byte.bit   Description                         Multiplier  Unit    Code
-1.          0x77 = 'w'
-2.          display Value                           100     -       BCD
-3.          display Value                           1       -       BCD
-4.0         unknown
-4.1         ?? Alarm 1 ??
-4.2         automatically switched to tap water refill
-4.3         manually switched to tap water refill
-4.4         unknown
-4.5         ?? Alarm 2 ??
-4.6         unknown
-4.7         unknown
-5.0         unknown
-5.1         unknown
-5.2         unknown
-5.3         unknown / this bit "flickers"
-5.4         unknown
-5.5         unknown
-5.6         display value is in percent
-5.7         display value is in qubic meters
+0.          0x77 = 'w'
+1.          display Value (low byte)                100     -       BCD
+2.          display Value (high byte)               1       -       BCD
 
-2020-09-17 00:13:40 - rcDisplay Alarm LED an, Dauerton aus, Nachspeisung LED dauerhaft an, Anzeige in cm
-Byte#:     01 02 03 04 05
-Daten:     77 40 01 25 0d
- Anzeige:                 140 cm
- manuelle Nachspeisung:   0
- Byte 4:                  0 0 1 0    0 1 0 1
- Byte 5:                  0 0 0 0    1 1 0 0
+3.0         OptionalLEDAlwaysOn
+3.1         OptionalLEDBlinking ?? Alarm 1 ??
+3.2         automatically switched to tap water refill
+3.3         manually switched to tap water refill
+3.4         water exchange active
+3.5         optional relais always on
+3.6         optional relais blinking
+3.7         buzzer active ?? in PROFIRAIN PCA documentation refered to bit 3.6 which should be for optional relais
 
-2020-09-17 00:18:40 - rcDisplay Alarm LED an, Dauerton aus, Nachspeisung LED dauerhaft an, Anzeige in %
-Byte#:     01 02 03 04 05
-Daten:     77 67 00 25 4d
- Anzeige:                 67 %
- manuelle Nachspeisung:   0
- Byte 4:                  0 0 1 0    0 1 0 1
- Byte 5:                  0 1 0 0    1 1 0 0
-
-2020-09-17 00:19:25 - rcDisplay Alarm LED an, Dauerton aus, Nachspeisung LED dauerhaft an, Anzeige in m3
-Byte#:     01 02 03 04 05
-Daten:     77 43 00 25 85
- Anzeige:                 4.3 m^3
- manuelle Nachspeisung:   0
- Byte 4:                  0 0 1 0    0 1 0 1
- Byte 5:                  1 0 0 0    0 1 0 0
-
-2020-09-17 00:26:53 - rcDisplay Alarm LED AUS, Dauerton aus, Nachspeisung LED AUS, Anzeige in m3
-Byte#:     01 02 03 04 05
-Daten:     77 43 00 00 82
- Anzeige:                 4.3 m^3
- manuelle Nachspeisung:   0
- Byte 4:                  0 0 0 0    0 0 0 0
- Byte 5:                  1 0 0 0    0 0 0 1
+4.0         optional input active
+4.1         tap water refill input active
+4.2         pump running
+4.3         unused / this bit "flickers/blinks"
+4.4         on button pressed
+4.5         off button pressed
+4.6         display value is in percent
+4.7         display value is in qubic meters
 
 */
 
@@ -119,7 +92,7 @@ enum eOptionalRelaisFunction{no_function,
                              leakage_warn_alarm,
                              reverse_leakage_warn_alarm,
                              parallel_pump,
-                             back_flushing,
+                             filter_back_flushing,
                              max_level_alarm,
                              min_level_alarm,
                              reservoir_empty_alarm,
@@ -150,8 +123,6 @@ class RCParameterMessage : public RCMessage
 // #Data :     70 30 07 01 03 01 42 00 31 01 30 06 51 51 97 00 00
 public:
     static const char msgIdentifier = 'p'; // 01. byte
-    // static const byte msgLength = 16;
-    // static constexpr byte msgExample[msgLength] = {0x70, 0x30, 0x07, 0x01, 0x03, 0x01, 0x42, 0x00, 0x31, 0x01, 0x30, 0x06, 0x51, 0x51, 0x97, 0x00};
     static const byte msgLength = 17;
     static constexpr byte msgExample[msgLength] = {0x70, 0x30, 0x07, 0x01, 0x03, 0x01, 0x42, 0x00, 0x31, 0x01, 0x30, 0x06, 0x51, 0x51, 0x97, 0x00, 0x00};
 
@@ -169,8 +140,8 @@ public:
     eReservoirType ReservoirType() const {return _ReservoirType;}
     float ReservoirArea_m2() const {return _ReservoirArea_m2;}
     eOptionalRelaisFunction OptionalRelaisFnuction() const {return _OptionalRelaisFunction;}
-    int AutomaticTimerInterval_days() const {return _AutomaticTimerInterval_days;}
-    int AutomaticTimerDuration_seconds() const {return _AutomaticTimerDuration_seconds;}
+    int FilterBackFlushingAutomaticTimerInterval_days() const {return _FilterBackFlushingAutomaticTimerInterval_days;}
+    int FilterBackFlushingAutomaticTimerDuration_seconds() const {return _FilterBackFlushingAutomaticTimerDuration_seconds;}
     int LevelCalibrationFactory() const {return _LevelCalibrationFactory;}
     int LevelCalibrationUser() const {return _LevelCalibrationUser;}
     int LevelMeasuredcm() const {return _LevelMeasured_cm;}
@@ -186,8 +157,8 @@ protected:
     eReservoirType _ReservoirType;
     float _ReservoirArea_m2;
     eOptionalRelaisFunction _OptionalRelaisFunction;
-    int _AutomaticTimerInterval_days;
-    int _AutomaticTimerDuration_seconds;
+    int _FilterBackFlushingAutomaticTimerInterval_days;
+    int _FilterBackFlushingAutomaticTimerDuration_seconds;
     int _LevelCalibrationFactory;
     int _LevelCalibrationUser;
     int _LevelMeasured_cm;
@@ -195,62 +166,66 @@ protected:
     float _Level_m3_Calibrated;
 };
 
-
-
 class RCDisplayMessage : public RCMessage
 {
 // Example in hex
-// #Byte#:   01 02 03 04 05
+// #Byte#:   00 01 02 03 04
 // #Data:    77 95 00 08 02
 public:
     static const char msgIdentifier = 'w'; // 01. byte
     static const byte msgLength = 5;
     static constexpr byte msgExample[msgLength] = {0x77, 0x95, 0x00, 0x08, 0x02};
+
     RCDisplayMessage();
     bool Decode(byte * msg, int msg_len);
     void operator=(const RCDisplayMessage &msg);
     bool operator==(const RCDisplayMessage &msg);
     bool operator!=(const RCDisplayMessage &msg);
+
     float DisplayValue() const {return _DisplayValue;}
     eDisplayUnit DisplayUnit() const {return _DisplayUnit;}
-    bool byte4_7() const {return _byte4_7;}
-    bool byte4_6() const {return _byte4_6;}
-    bool Alarm_2() const {return _Alarm_2;}
-    bool byte4_4() const {return _byte4_4;}
-    bool ManualSwitchedToTapWater() const {return _ManualSwitchedToTapWater;}
-    bool AutomaticallySwitchedToTapWater() const {return _AutomaticallySwitchedToTapWater;}
-    bool IsSwitchedToTapWater();
-    bool Alarm_1() const {return _Alarm_1;}
-    bool byte4_0() const {return _byte4_0;}
+    bool IsSwitchedToTapWater() {return (ManualSwitchedToTapWater() || AutomaticallySwitchedToTapWater() || WaterExchangeActive());}
 
-    bool QubicMeters() const {return _QubicMeters;}
-    bool Percent() const {return _Percent;}
-    bool byte5_5() const {return _byte5_5;}
-    bool byte5_4() const {return _byte5_4;}
-    bool byte5_3() const {return _byte5_3;}
-    bool byte5_2() const {return _byte5_2;}
-    bool byte5_1() const {return _byte5_1;}
-    bool byte5_0() const {return _byte5_0;}
+    // byte 3 decodings
+    bool OptionalLEDAlwaysOn() const {return _OptionalLEDAlwaysOn;} // byte 3.0
+    bool OptionalLEDBlinking() const {return _OptionalLEDBlinking;} // byte 3.1
+    bool AutomaticallySwitchedToTapWater() const {return _AutomaticallySwitchedToTapWater;}  // byte 3.2
+    bool ManualSwitchedToTapWater() const {return _ManualSwitchedToTapWater;}  // byte 3.3
+    bool WaterExchangeActive() const {return _WaterExchangeActive;} // byte 3.4
+    bool OptionalRelaisAlwaysOn() const {return _OptionalRelaisAlwaysOn;} // byte 3.5
+    bool OptionalRelaisBlinking() const {return _OptionalRelaisBlinking;} // byte 3.6
+    bool AlarmBuzzerActive() const {return _AlarmBuzzerActive;} // byte 3.7
+
+    // byte 4 decodings
+    bool OptionalInputActive() const {return _OptionalInputActive;}
+    bool TapWaterRefillInputActive() const {return _TapWaterRefillInputActive;}
+    bool PumpActive() const {return _PumpActive;}
+    bool unused() const {return _unused;}
+    bool OnButtonPressed() const {return _OnButtonPressed;}
+    bool OffButtonPressed() const {return _OffButtonPressed;}
+    bool DisplayInPercent() const {return _DisplayInPercent;}
+    bool DisplayInQubicMeters() const {return _DisplayInQubicMeters;}
+
 protected:
     float _DisplayValue;
     eDisplayUnit _DisplayUnit;
-    bool _byte4_7;
-    bool _byte4_6;
-    bool _Alarm_2;
-    bool _byte4_4;
+    bool _AlarmBuzzerActive;
+    bool _OptionalRelaisBlinking;
+    bool _OptionalRelaisAlwaysOn;
+    bool _WaterExchangeActive;
     bool _ManualSwitchedToTapWater;
     bool _AutomaticallySwitchedToTapWater;
-    bool _Alarm_1;
-    bool _byte4_0;
+    bool _OptionalLEDBlinking;
+    bool _OptionalLEDAlwaysOn;
 
-    bool _QubicMeters;
-    bool _Percent;
-    bool _byte5_5;
-    bool _byte5_4;
-    bool _byte5_3;
-    bool _byte5_2;
-    bool _byte5_1;
-    bool _byte5_0;
+    bool _DisplayInQubicMeters;
+    bool _DisplayInPercent;
+    bool _OffButtonPressed;
+    bool _OnButtonPressed;
+    bool _unused;
+    bool _PumpActive;
+    bool _TapWaterRefillInputActive;
+    bool _OptionalInputActive;
 
 };
 
