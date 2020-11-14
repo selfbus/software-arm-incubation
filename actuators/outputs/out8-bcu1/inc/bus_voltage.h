e25b8119aa2f4ee13db631a10b7876db816ca14d
Darthyson <darth@maptrack.de>
2020-11-14 17:58:49 +0100

    out8-bcu1: added howto use

--- actuators/outputs/out8-bcu1/inc/bus_voltage.h
+++ actuators/outputs/out8-bcu1/inc/bus_voltage.h
@@ -1,20 +1,66 @@
 /*
  *  bus_voltage.h - simple ADC ISR monitoring of the bus voltage on the vref-pin of the LPCxx
  *
- *  Don't create instances of class BusVoltage, use variable vBus instead
- *
- *  Implement in your apps main:
- *
- *  void BusVoltageFail()
- *  void BusVoltageReturn()
- *
- *  both will be called by the ISR which handles the ADC interrupt for the bus voltage.
- *
  *  Copyright (c) 2020 Darthyson <darth@maptrack.de>
  *
  *  This program is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License version 3 as
  *  published by the Free Software Foundation.
+ *  Don't create instances of class BusVoltage, use variable vBus instead
+ *
+ *  Usage:
+ *      implement & define:
+ *      --------------------------------------------------------------------------------------------------------------*
+ *
+ *      #define VBUS_AD_CHANNEL AD7 // ARM-ADC Channel for measuring the bus voltage
+ *      #define VBUS_THRESHOLD 1.94 // voltage threshold below which a bus failure should be reported
+ *      void BusVoltageFail()       //  will be called by the ISR which handles the ADC interrupt for the bus voltage.
+ *      void BusVoltageReturn()     //  will be called by the ISR which handles the ADC interrupt for the bus voltage.
+ *
+ *      --------------------------------------------------------------------------------------------------------------
+ *      e.g. in your app_main.cpp:
+ *
+ *        #define VBUS_AD_CHANNEL AD7
+ *        #define VBUS_THRESHOLD 1.94
+ *
+ *        void setup()
+ *        {
+ *            .... your code ....
+ *
+ *            // enable bus voltage monitoring on PIO1_11 & AD7 with 1.94V threshold
+ *            vBus.enableBusVRefMonitoring(PIN_VBUS, VBUS_AD_CHANNEL, VBUS_THRESHOLD);
+ *
+ *            .... your code ....
+ *        }
+ *
+ *        void loop()
+ *        {
+ *            .... your code ....
+ *
+ *            // the bus voltage check, should be done before waitForInterrupt()
+ *            vBus.checkPeriodic();
+ *            if (bus.idle())
+ *            {
+ *                waitForInterrupt();
+ *            }
+ *        }
+ *
+ *        void BusVoltageFail()       //  will be called by the ISR in case of bus voltage failure.
+ *        {
+ *            .... your code for a bus failure ....
+ *        }
+ *
+ *        void BusVoltageReturn()     //  will be called by the ISR in case of bus voltage return.
+ *        {
+ *            .... your code for a bus return ....
+ *        }
+ *      --------------------------------
+ *
+ *  1.94V @ ADC-Pin of the LPC11xx is just selected for fast testing, needs further investigation
+ *  depend's on used controller e.g.
+ *  4TE-ARM the voltage divider is R3/R12 (91K0 & 10K0)
+ *  TS_ARM  the voltage divider is R3/R12 (91K0 & 10K0)
+ *
  */
 
 #ifndef BUS_VOLTAGE_H_
