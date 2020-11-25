e670efb2d01aece70ba991d64616e8f23bd30a81
Darthyson <darth@maptrack.de>
2020-11-25 02:08:53 +0100

    out8-bcu1: code optimization, added info for bi-stable relays

--- actuators/outputs/out8-bcu1/src/outputsBiStable.cpp
+++ actuators/outputs/out8-bcu1/src/outputsBiStable.cpp
@@ -10,23 +10,56 @@
  */
 
 #include "outputsBiStable.h"
-#define ON_DELAY 10
+
+#define ON_DELAY 10 // Hongfa HFE20-1 24-1HSD-L2(359) datasheet
+                    // says "Pulse Duration ms min. 50ms"
+                    // tested with:
+                    //  5ms,  of 8 relays work //FIXME do the testing!
+                    //  6ms,  of 8 relays work
+                    //  7ms,  of 8 relays work
+                    //  8ms,  of 8 relays work
+                    //  9ms,  of 8 relays work
+                    // 10ms,  of 8 relays work
+
+/*
+ * order code Hongfa HFE20-1 24-1HSD-L2(359)
+ *    1:      5mm pin
+ *   24:     24VDC
+ *   1H:     1 Form A
+ *    S:      Plastic sealed
+ *    D:      W+AgSnO2 Contact
+ *   L2:     Double coils latching
+ *    -:    Positive polarity
+* (359):  lamp load
+ */
+
+#define pinOff(i) (i*2+1)
+#define pinOn(i)  (i*2)
+
+
 
 void OutputsBiStable::updateOutputs(void)
 {
     unsigned int mask = 0x01;
     unsigned int i;
     unsigned int state   = _relayState ^ _inverted;
-	unsigned int changes = _relayState ^ _prevRelayState;
+    unsigned int changes = _relayState ^ _prevRelayState;
 
     for (i = 0; i < NO_OF_CHANNELS; i++, mask <<= 1)
     {
-    	if (changes & mask)
-    	{
-    	    if (state & mask)
-                 digitalWrite(outputPins[i*2],     1);
-            else digitalWrite(outputPins[i*2 + 1], 1);
-    	    _pwm_timeout.start(ON_DELAY);
+        if (changes & mask)
+        {
+            if (state & mask)
+            {
+                digitalWrite(outputPins[pinOff(i)], 0);
+                digitalWrite(outputPins[pinOn(i)],  1);
+            }
+            else
+            {
+                digitalWrite(outputPins[pinOn(i)],  0);
+                digitalWrite(outputPins[pinOff(i)], 1);
+            }
+            _pwm_timeout.start(ON_DELAY);
 #ifdef HAND_ACTUATION
             handAct.setLedState(i, state & mask);
 #endif
@@ -39,7 +72,7 @@ void OutputsBiStable::checkPWM(void)
 {
     if (_pwm_timeout.started () && _pwm_timeout.expired ())
     {
-        for (unsigned int i = 0; i < NO_OF_OUTPUTS; i++)
+        for (unsigned int i = 0; i < sizeof(outputPins)/sizeof(outputPins[0]); i++)
             digitalWrite (outputPins[i], 0);
     }
 }
