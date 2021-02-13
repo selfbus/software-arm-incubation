024fe16abaed1ea04772fad8256604fc59cc83f6
Darthyson <darth@maptrack.de>
2021-02-13 18:16:25 +0100

    common hand-actuation: changed constructor HandActuation, BREAKS compiling of out8-bcu1 and rol-jal-bim112 !!!
    changed constructor HandActuation to take hand actuation configuration as parameters, so we can get rid of "extern const int handPins[NO_OF_HAND_PINS];"

--- common/hand-actuation/hand_actuation.cpp
+++ common/hand-actuation/hand_actuation.cpp
@@ -1,5 +1,6 @@
 /*
  *  Copyright (c) 2015 Martin Glueck <martin@mangari.org>
+ *                2021 Darthyson <darth@maptrack.de>
  *
  *  This program is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License version 3 as
@@ -11,100 +12,99 @@
 #include <sblib/timer.h>
 #include <sblib/io_pin_names.h>
 
-/*
- *  hand actuation pin configuration
- */
-#ifdef HAND_ACTUATION
-    const int handPins[NO_OF_HAND_PINS] =
-           // LedTaster_4TE_3.54 : links oben = Kanal 1 nach unten aufsteigende Kanalnummer ; rechts oben = Kanal 5 ; nach unten aufsteigende Kanalnummer
-           { PIN_LT1, PIN_LT2, PIN_LT3, PIN_LT4, PIN_LT5, PIN_LT6, PIN_LT7, PIN_LT8 };
-           // Pinbelegung für Hardware ???
-           // for weatherstation-bim112? see commit 870c9801bea5aa616c22fde29c8219e4cb5f28b6
-           // { PIN_LT5, PIN_LT6, PIN_LT1, PIN_LT2, PIN_LT3, PIN_LT4, PIN_LT7, PIN_LT8 };
-#endif
-#define HAND_READBACK PIN_LT9
-#define BLINK_TIME 500
-
 HandActuation::HandActuation()
-   : number(0)
-   , mask(0x01)
-   , _buttonState(0)
-   , _ledState(0)
-   , _blinkState(0)
+   : number_(0)
+   , mask_(0x01)
+   , buttonState_(0)
+   , ledState_(0)
+   , blinkState_(0)
 {
-    for (unsigned int i = 0; i < GetHandPinCount(); i++)
+}
+
+HandActuation::HandActuation(const unsigned int* Pins, const unsigned int pinCount, const unsigned int readbackPin, const unsigned int blinkTimeMs)
+    : number_(0)
+    , mask_(0x01)
+    , buttonState_(0)
+    , ledState_(0)
+    , blinkState_(0)
+{
+    handPins_ = (unsigned int*) Pins;
+    pinCount_ = pinCount;
+    for (unsigned int i = 0; i < pinCount; i++)
     {
-        pinMode(handPins[i], OUTPUT);
-        digitalWrite(handPins[i], false);
+        pinMode(Pins[i], OUTPUT);
+        digitalWrite(Pins[i], false);
     }
 
-    pinMode(HAND_READBACK, PULL_UP);
+    readbackPin_ = readbackPin;
+    pinMode(readbackPin_, PULL_UP);
 
-    blinkTimer.start(BLINK_TIME);
+    blinkTimeMs_ = blinkTimeMs;
+    blinkTimer.start(blinkTimeMs_);
 }
 
 int HandActuation::check(void)
 {
     int result = NO_ACTUATION;
-    if (_handDelay.expired() || _handDelay.stopped())
+    if (handDelay_.expired() || handDelay_.stopped())
     {   // check one input at a time
         bool buttonundertestingispressed = false;
 
         // save led state and turn off the LED for the button we are testing
-        bool lastLEDState = _ledState & (1 << number);
+        bool lastLEDState = ledState_ & (1 << number_);
         if (lastLEDState)
-            digitalWrite(handPins[number], false);
+            digitalWrite(handPins_[number_], false);
 
-        bool atleastonebuttonpressed = !digitalRead(HAND_READBACK); // read while the LED is off, low=>at least one button is pressed, high=>no button is pressed
+        bool atleastonebuttonpressed = !digitalRead(readbackPin_); // read while the LED is off, low=>at least one button is pressed, high=>no button is pressed
 
         if (atleastonebuttonpressed) // at least one button is pressed, check if its the one we are testing right now (number)
         {
             // turn on all LED's, except the one for the button under testing (number)
             for (unsigned int i = 0; i < GetHandPinCount(); i++)
-                if (i != number) digitalWrite(handPins[i], true);
+                if (i != number_) digitalWrite(handPins_[i], true);
             delayMicroseconds(5);   // this delay is needed for compiler settings other then -O0 (Optimize Level None), otherwise detection doesn't work 100%
                                     // works also with delayMicroseconds(1);
-            buttonundertestingispressed = !digitalRead(HAND_READBACK); // read while all LED's are on, except the one for our button we check right now, low=>button to check is pressed
+            buttonundertestingispressed = !digitalRead(readbackPin_); // read while all LED's are on, except the one for our button we check right now, low=>button to check is pressed
 
             // restore LED states
             for (unsigned int i = 0, bitMask = 0x01; i < GetHandPinCount(); i++, bitMask = bitMask << 1)
             {
-                if (_blinkState & bitMask)
-                    digitalWrite(handPins[i], blinkOnOffState);
+                if (blinkState_ & bitMask)
+                    digitalWrite(handPins_[i], blinkOnOffState);
                 else
-                    digitalWrite(handPins[i], _ledState & bitMask);
+                    digitalWrite(handPins_[i], ledState_ & bitMask);
             }
         }
         else
         {
             // restore LED status for the button under testing
-            if ((_blinkState & (1 << number)))
-                digitalWrite(handPins[number], blinkOnOffState);
+            if ((blinkState_ & (1 << number_)))
+                digitalWrite(handPins_[number_], blinkOnOffState);
             else
-                digitalWrite(handPins[number], lastLEDState);
+                digitalWrite(handPins_[number_], lastLEDState);
         }
 
         if (buttonundertestingispressed)
         {   // this button is currently pressed
-            result = number;
-            if (! (_buttonState & mask))
+            result = number_;
+            if (! (buttonState_ & mask_))
                 // this button was not pressed before
                 result |= BUTTON_PRESSED;
-            _buttonState |= mask;
+            buttonState_ |= mask_;
         }
-        else if (_buttonState & mask)
+        else if (buttonState_ & mask_)
         {
-            result = number | BUTTON_RELEASED;
-            _buttonState &= ~mask;
+            result = number_ | BUTTON_RELEASED;
+            buttonState_ &= ~mask_;
         }
-        number++;
-        mask <<= 1;
-        if (number == GetHandPinCount())
+        number_++;
+        mask_ <<= 1;
+        if (number_ == GetHandPinCount())
         {
-            number = 0;
-            mask  = 0x1;
+            number_ = 0;
+            mask_  = 0x1;
         }
-        _handDelay.start(DELAY_BETWEEN_BUTTONS);
+        handDelay_.start(delayBetweenButtonsMs);
     } // if (_handDelay.expired()
 
 
@@ -112,11 +112,11 @@ int HandActuation::check(void)
     {
         for (unsigned int i = 0, bitMask = 0x01; i < GetHandPinCount(); i++, bitMask = bitMask << 1)
         {
-            if (_blinkState & bitMask)
-                digitalWrite(handPins[i], blinkOnOffState);
+            if (blinkState_ & bitMask)
+                digitalWrite(handPins_[i], blinkOnOffState);
         }
         blinkOnOffState = !blinkOnOffState;
-        blinkTimer.start(BLINK_TIME);
+        blinkTimer.start(blinkTimeMs_);
     }
     return result;
 }
@@ -137,10 +137,7 @@ int HandActuation::getButtonAndState(int& btnNumber, HandActuation::ButtonState&
 
 ALWAYS_INLINE unsigned int HandActuation::GetHandPinCount()
 {
-    if (sizeof(handPins) > 0)
-        return (sizeof(handPins)/sizeof(handPins[0]));
-    else
-        return -1;
+   return pinCount_;
 }
 
 void HandActuation::setLedState(unsigned int led, bool state, bool blink)
@@ -149,17 +146,17 @@ void HandActuation::setLedState(unsigned int led, bool state, bool blink)
         return;
 
     unsigned int mask = 1 << led;
-    digitalWrite(handPins[led], state);
+    digitalWrite(handPins_[led], state);
     if (state)
     {
-        _ledState |= mask;
+        ledState_ |= mask;
         if (blink)
-            _blinkState |= mask;
+            blinkState_ |= mask;
     }
     else
     {
-        _ledState   &= ~mask;
-        _blinkState &= ~mask;
+        ledState_   &= ~mask;
+        blinkState_ &= ~mask;
     }
 }
 
@@ -171,8 +168,13 @@ void HandActuation::setallLedState(bool state)
     }
 }
 
-#ifdef HAND_ACTUATION
-    HandActuation handAct = HandActuation();
-    Timeout HandActuation::blinkTimer;
-    bool    HandActuation::blinkOnOffState = false;
-#endif
+
+Timeout HandActuation::blinkTimer;
+bool    HandActuation::blinkOnOffState = false;
+/*
+#   define NO_OF_HAND_PINS 8
+#   define READBACK_PIN PIN_LT9
+#   define BLINK_TIME 500
+    unsigned int handPins[NO_OF_HAND_PINS] = { PIN_LT1, PIN_LT2, PIN_LT3, PIN_LT4, PIN_LT5, PIN_LT6, PIN_LT7, PIN_LT8 };
+HandActuation handAct = HandActuation(&handPins[0], NO_OF_HAND_PINS, READBACK_PIN, BLINK_TIME);
+*/
--- common/hand-actuation/hand_actuation.h
+++ common/hand-actuation/hand_actuation.h
@@ -1,5 +1,6 @@
 /*
  *  Copyright (c) 2015 Martin Glueck <martin@mangari.org>
+ *                2021 Darthyson <darth@maptrack.de>
  *
  *  This program is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License version 3 as
@@ -9,21 +10,13 @@
 #ifndef HAND_ACTUATION_H_
 #define HAND_ACTUATION_H_
 
-#include "config.h"
 #include <sblib/timeout.h>
-
-#ifndef NO_OF_HAND_PINS
-#   define NO_OF_HAND_PINS NO_OF_CHANNELS //FIXME modules should work without external symbols/variables
-#   pragma message ( "FIXME NO_OF_HAND_PINS not defined!, defaulting to NO_OF_CHANNELS" )
-#endif
-
-extern const int handPins[NO_OF_HAND_PINS];
 
 class HandActuation
 {
 public:
     enum ButtonState {NO_ACTUATION = -1, BUTTON_PRESSED = 0x100, BUTTON_RELEASED = 0x200};
-    HandActuation();
+    HandActuation(const unsigned int* Pins, const unsigned int pinCount, const unsigned int readbackPin, const unsigned int blinkTimeMs);
     int check(void);
     int getButtonAndState(int& btnNumber, HandActuation::ButtonState& btnState); // returns true in case a button is pressed or was released
     bool ledState(unsigned int led);
@@ -32,29 +25,34 @@ public:
     void setallLedState(bool state);
 
 protected:
-    static const unsigned int DELAY_AT_END          = 10;
-    static const unsigned int DELAY_BETWEEN_BUTTONS = 10;
+    static const unsigned int delayBetweenButtonsMs = 10;
     static Timeout blinkTimer;
     static bool blinkOnOffState;
     unsigned int GetHandPinCount();
+    unsigned int* handPins_;
+    unsigned int pinCount_;
+    unsigned int readbackPin_;
+    unsigned int blinkTimeMs_;
 
-    unsigned int  number;
-    unsigned char mask;
-    unsigned char _buttonState;
-    unsigned char _ledState;
-    unsigned char _blinkState;
-    Timeout _handDelay;
+    unsigned int  number_;
+    unsigned char mask_;
+    unsigned char buttonState_;
+    unsigned char ledState_;
+    unsigned char blinkState_;
+    Timeout handDelay_;
+private:
+    HandActuation();
 };
 
-extern HandActuation handAct;
+// extern HandActuation handAct;
 
 inline bool HandActuation::ledState(unsigned int led)
 {
-    return _ledState & (1 << led) ? true : false;
+    return ledState_ & (1 << led) ? true : false;
 }
 
 inline bool HandActuation::blinkState(unsigned int led)
 {
-    return _blinkState & (1 << led) ? true : false;
+    return blinkState_ & (1 << led) ? true : false;
 }
 #endif /* HAND_ACTUATION_H_ */
