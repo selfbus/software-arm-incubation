228d07712400abb1b56d32f551c730a0a1746d18
Darthyson <darth@maptrack.de>
2022-02-16 03:33:51 +0100

    [rol-jal-bim] added todos, moved noApp code to loop_noapp

--- actuators/blind-shutter/rol-jal-bim112/src/app_main.cpp
+++ actuators/blind-shutter/rol-jal-bim112/src/app_main.cpp
@@ -17,15 +17,11 @@
 #include <string.h>
 
 #ifdef HAND_ACTUATION
-#include "hand_actuation.h"
+#   include "hand_actuation.h"
 #endif
 
-// create APP_VERSION, its used in the bus updater magic string is !AVP!@:
-// from Rauchmelder-bcu1 (app_main.cpp):
+///\todo Replace APP_VERSION[20] and getAppVersion with new sblib macro APP_VERSION("SBrol   ", "1", "01");
 volatile const char __attribute__((used)) APP_VERSION[20] = "!AVP!@:SBrol   1.01";
-// disable optimization seems to be the only way to ensure that this is not being removed by the linker
-// to keep the variable, we need to declare a function that uses it
-// alternatively, the link script may be modified by adding KEEP to the section
 volatile const char * __attribute__((optimize("O0"))) getAppVersion()
 {
     return APP_VERSION;
@@ -33,23 +29,27 @@ volatile const char * __attribute__((optimize("O0"))) getAppVersion()
 
 // Hardware version. Must match the product_serial_number in the VD's table hw_product
 const HardwareVersion hardwareVersion[] =
-{ {4, 0x4578, { 0, 0, 0, 0, 0x0, 0x29 }}
-, {8, 0x46B8, { 0, 0, 0, 0, 0x0, 0x28 }}
+{
+    ///\todo implement missing 1, 2, 8- fold versions
+    {4, 0x4578, { 0, 0, 0, 0, 0x0, 0x29 }} // JAL-0410.01 Shutter Actuator 4-fold, 4TE, 230VAC, 10A
+    // {8, 0x46B8, { 0, 0, 0, 0, 0x0, 0x28 }}  // JAL-0810.01 Shutter Actuator 8-fold, 8TE, 230VAC,10A
 };
 
 const HardwareVersion * currentVersion;
 
 Timeout timeout;
 
-/*
- * Initialize the application.
+/**
+ * This function is called by the Selfbus's library main when the processor is started or reset.
  */
 void setup()
 {
-    // XXX read some ID pins to determine which version is attached
+    ///\todo read some ID pins to determine which version is attached
+
     currentVersion = & hardwareVersion[0];
     volatile const char * v = getAppVersion();      // Ensure APP ID is not removed by linker (its used in the bus updater)
     v++;                                            // just to avoid compiler warning of unused variable
+
     bcu.begin(131, currentVersion->hardwareVersion[5], 0x28);  // we are a MDT shutter/blind actuator, version 2.8
     memcpy(userEeprom.order, currentVersion->hardwareVersion, sizeof(currentVersion->hardwareVersion));
 
@@ -64,23 +64,12 @@ void setup()
 	digitalWrite(PIN_RUN, 0);
 #endif
 
-
-    if (bcu.applicationRunning())
-    {
-        initApplication();
-    }
-    else
-    {
-        // no application is loaded, make sure that all relays are off
-        pinMode(PIN_PWM, OUTPUT);  // configure PWM Pin as output when app is not loaded
-        digitalWrite(PIN_PWM, 1);  // set PWM Pin to high so all relays will be off
-    }
-
-	timeout.start    (1);
+    initApplication();
+	timeout.start(1);
 }
 
-/*
- * The main processing loop.
+/**
+ * The main processing loop while a KNX-application is loaded
  */
 void loop()
 {
@@ -90,11 +79,21 @@ void loop()
     {
         objectUpdated(objno);
     }
-    // check the periodic function of the application
+    // check the periodic functions of the application
     checkPeriodicFuntions();
 
     // Sleep up to 1 millisecond if there is nothing to do
     if (bus.idle())
         waitForInterrupt();
+}
+
+/**
+ * The processing loop while no KNX-application is loaded
+ */
+void loop_noapp()
+{
+    // no application is loaded, make sure that all relays are off
+    pinMode(PIN_PWM, OUTPUT);  // configure PWM Pin as output when application is not loaded
+    digitalWrite(PIN_PWM, 1);  // set PWM Pin to high so all relays will be off
 }
 
