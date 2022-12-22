selfbus/software-arm-incubation

commit 091d9c9e2b5b4ea637c76473aab7a5e120e83a38

[in16-bim112] refactored some INVERT handling for better readability
-added more build configs

sensors/binary-inputs/in16-bim112/src/input.cpp
@@ -21,10 +21,11 @@ void Input::begin(int noOfChannels, int baseAddress)
     int mode;
     unsigned int mask = 0;
 
+    mode = INPUT | HYSTERESIS;
 #ifdef INVERT
-    	mode = INPUT | HYSTERESIS | PULL_UP;
+    mode |= PULL_UP;
 #else
-    	mode = INPUT | HYSTERESIS | PULL_DOWN;
+    mode |= PULL_DOWN;
 #endif
 
     for (int i = 0; i < noOfChannels; i++)
@@ -40,28 +41,21 @@ void Input::begin(int noOfChannels, int baseAddress)
 
 void Input::scan(void)
 {
-    for (unsigned int i = 0; i < noOfChannels; i++)
+    bool pinState;
+    for (uint8_t i = 0; i < noOfChannels; i++)
     {
+        pinState = digitalRead(inputPins[i]);
 #ifdef INVERT
-      	if (digitalRead(inputPins[i]))
-       	{
-       		inputState &= 0xffff ^ (1 << i);
-       	}
-       	else
-       	{
-       		inputState |= 1 << i;
-       	}
-
-#else
-       	if (digitalRead(inputPins[i]))
-       	{
-       		inputState |= 1 << i;
-       	}
-       	else
-       	{
-       		inputState &= 0xffff ^ (1 << i);
-       	}
+        pinState = !pinState;
 #endif
+      	if (pinState)
+       	{
+       		inputState &= 0xffff ^ (1 << i);
+       	}
+       	else
+       	{
+       		inputState |= 1 << i;
+       	}
     }
 }
 

sensors/binary-inputs/in16-bim112/src/dimmer.cpp
@@ -23,7 +23,7 @@ Dimmer::Dimmer(unsigned int no, unsigned int longPress,
 
     debug_eeprom("Channel EEPROM:", channelConfig, 46);
 
-    if (!bcu.userEeprom->getUInt8(channelConfig + 0x20) & 0x3)
+    if (!(bcu.userEeprom->getUInt8(channelConfig + 0x20) & 0x3))
     {
         repeatDimmerTime = 0;
     }

commit 08e05fc43b4fc4ec7d96a2dc212fe82f5cca4462

[in16-bim112] fixed INVERT handling
-bug introduced with commit 091d9c9e2b5b4ea637c76473aab7a5e120e83a38

sensors/binary-inputs/in16-bim112/src/input.cpp
@@ -48,14 +48,14 @@ void Input::scan(void)
 #ifdef INVERT
         pinState = !pinState;
 #endif
-      	if (pinState)
-       	{
-       		inputState &= 0xffff ^ (1 << i);
-       	}
-       	else
-       	{
-       		inputState |= 1 << i;
-       	}
+        if (pinState)
+        {
+            inputState |= 1 << i;
+        }
+        else
+        {
+            inputState &= 0xffff ^ (1 << i);
+        }
     }
 }
 