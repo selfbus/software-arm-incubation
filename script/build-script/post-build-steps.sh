c0289af4ea736396220101eab3288be95f442e5e
Darthyson <darth@maptrack.de>
2023-04-27 17:59:58 +0200

    [sblib] Moved post-build steps to external shell script
    The post-build-steps.sh script does following steps:
    - normalize and lowercase `${BuildArtifactFileBaseName}`
    - add the sblib version to the .bin/.hex filename
    - create the .bin/.hex files from the `${BuildArtifactFileName}`
    - copy the .hex file to sub folder (debug, release) of the workspace build variable `${hexDir}`

---
+++ script/build-script/post-build-steps.sh
@@ -0,0 +1,101 @@
+#!/bin/bash
+
+# Configure in MCUXpresso in the workspace a build variable for the .hex output directory
+# 'Window->Preferences->C/C++->Build->Build Variables->Add...'
+# Variable name: hexDir
+# Type:          String
+# Value:         absolute path to .hex output directory
+
+# Configure in MCUXpresso for ALL build configurations:
+
+# 'Project->Properties->C/C++ Build->Build Variables->Add...'
+# Variable name: sw_version
+# Type:          String
+# Value:         x.xx
+
+# 'Project->Properties->C/C++ Build->Settings->Build Artifact->Artifact name:'
+# ${ProjName}_${ConfigName}_v${sw_version}
+
+# 'Project->Properties->C/C++ Build->Settings->Build steps->Post-build steps->Edit...'
+# "${ProjDirPath}/../../../script/build-script/post-build-steps.sh" "${CWD}" "${TargetChip}" "${BuildArtifactFileName}" "${BuildArtifactFileBaseName}" "${hexDir}"
+
+# Selfbus library version
+sbLibVersion="2.02"
+
+# Selfbus library version prefix
+sbLibprefix="libv"
+
+# searchstrings and subfolders for .hex output directory
+debug="debug"
+release="release"
+flashstart="flashstart"
+
+CurrentWorkingDirectory="${1}"
+TargetChip="${2}"
+BuildArtifactFileName="${3}"
+BuildArtifactFileBaseName="${4}"
+hexDir="${5}"
+
+#echo "BuildArtifactFileBaseName "${BuildArtifactFileBaseName}""
+
+echo ""
+echo "Selfbus post build steps:"
+echo "CWD "${CurrentWorkingDirectory}""
+echo "build \""${BuildArtifactFileName}"\""
+echo "mcu "${TargetChip}""
+echo "Creating .hex/.bin files and adding sblib version "${sbLibVersion}" to filename"
+
+# replace spaces with underscores
+newName="${BuildArtifactFileBaseName// /_}"
+# replace missing options (--)
+newName="${newName//--/_}"
+# fix trailing missing option
+newName="${newName//-_/_}"
+# fix leading missing option
+newName="${newName//_-/_}"
+# replace duplicate underscores
+newName="${newName//__/_}"
+# convert to lowercase
+newName=`echo "${newName}" | tr '[:upper:]' '[:lower:]'`
+# add lib version
+newName="${newName}_${sbLibprefix}${sbLibVersion}"
+echo "${newName}"
+
+# post-build-step from eclipse
+arm-none-eabi-size "${BuildArtifactFileName}"
+# create .bin file
+arm-none-eabi-objcopy -v -O binary "${BuildArtifactFileName}" "${newName}.bin"
+# create .hex file
+arm-none-eabi-objcopy -v -O ihex "${BuildArtifactFileName}" "${newName}.hex"
+
+# add release/debug to hex output directory
+case ${newName} in
+  *"${debug}"*)
+    hexDir="${hexDir}"/"${debug}"
+    ;;
+
+  *"${release}"*)
+    hexDir="${hexDir}"/"${release}"
+    ;;
+esac
+
+# check adding flashstart to hex output directory
+if [[ "$newName" =~ .*"$flashstart".* ]]; then
+  echo "flashstart"
+  hexDir="${hexDir}"/"${flashstart}"
+fi
+
+# check for .hex output directory
+if [ ! -d "${hexDir}" ]; then
+  echo "Creating "${hexDir}""
+  mkdir -p "${hexDir}"
+fi
+
+cp "${CurrentWorkingDirectory}"/"${newName}.hex" "${hexDir}"/
+
+# Do not activate checksum, not sure why, but at least .hex files get corrupted
+# see also https://community.nxp.com/t5/Blogs/Hex-file-settings-in-MCUxpresso/bc-p/1131124/highlight/true#M53
+# add checksums
+#checksum -p ${TargetChip} -d "${newName}.bin"
+#checksum -p ${TargetChip} -d "${newName}.hex"
+
