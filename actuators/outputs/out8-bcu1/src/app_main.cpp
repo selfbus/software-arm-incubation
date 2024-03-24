f4e88f8f42d6d162cfe0531ab0c6d5fe3bfda4fa
Darthyson <darth@maptrack.de>
2024-03-24 17:39:09 +0100

    [out8] Raise version to 5.11

--- actuators/outputs/out8-bcu1/src/app_main.cpp
+++ actuators/outputs/out8-bcu1/src/app_main.cpp
@@ -40,7 +40,7 @@
     AppUsrCallback usrCallback; ///\todo two callbacks? Optimize with busfail integration into the sblib.
 #endif
 
-APP_VERSION("O08.10  ", "5", "10");
+APP_VERSION("O08.10  ", "5", "11");
 
 /**
  * Simple IO test
