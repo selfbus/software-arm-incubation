360ee2723f2802c2c87883f1262113595abf03fd
Darthyson <darth@maptrack.de>
2024-07-12 01:11:08 +0200

    [USB-IF] bcu.begin checks for 0 and sets already to default KNX address 15.15.255
    No need to disable code read protection. Its already disabled in project settings.

--- misc/USB-Interface-bcu1/USB-IF_Knx/src/app_main.cpp
+++ misc/USB-Interface-bcu1/USB-IF_Knx/src/app_main.cpp
@@ -18,9 +18,6 @@
 #include "device_mgnt.h"
 #include "prog_uart.h"
 
-// Code Protection deaktiviert
-__attribute__ ((used,section(".crp"))) const unsigned int CRP_WORD = 0xFFFFFFFF;
-
 BCU1 bcu = BCU1();
 
 APP_VERSION("SBif_knx", "1", "10") // Don't forget to also change the build-variable sw_version
@@ -35,14 +32,6 @@ BcuBase* setup()
     digitalWrite(PIN_PROG, true);
     digitalWrite(PIO1_5, true);
     bcu.begin(2, 1, 1); // ABB, dummy something device
-
-    /* ///\todo why? bcu.begin checks for 0 and sets to default KNX address 15.15.255, 1.1.1 will most likely already be used
-    int addr = bcu.ownAddress();
-    if ((addr == 0) || (addr == 0xffff))
-    {
-        bcu.setOwnAddress(0x1101); // default 1.1.1
-    }
-     */
     uart.Init(115200, false);
     return (&bcu);
 }
@@ -66,7 +55,7 @@ BcuBase* setup()
 void loop()
 {
   uart.SerIf_Tasks();
-  // emiknxif.SetCdcMonMode(true); //only for debug to "hard" activate busmonitor mode
+  // emiknxif.SetCdcMonMode(true); //only for debugging to "hard" activate serial busmonitor mode
   emiknxif.EmiIf_Tasks();
   proguart.SerIf_Tasks();
   devicemgnt.DevMgnt_Tasks();
