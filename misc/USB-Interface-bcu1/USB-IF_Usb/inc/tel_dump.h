aeb58baa5ab3661424bc871728d29a951ce55cd3
Darthyson <darth@maptrack.de>
2025-01-30 19:11:16 +0100

    [USB-IF_Usb] Use 4 spaces as indent

--- misc/USB-Interface-bcu1/USB-IF_Usb/inc/tel_dump.h
+++ misc/USB-Interface-bcu1/USB-IF_Usb/inc/tel_dump.h
@@ -16,20 +16,20 @@
 class TelDump
 {
 public:
-	int Dump(unsigned int time, bool DirSend, unsigned tellen, uint8_t* telptr);
+    int Dump(unsigned int time, bool DirSend, unsigned tellen, uint8_t* telptr);
 protected:
-	unsigned linelen;
-	unsigned remlen;
-	char* linebuffer;
-	void buf_printf(const char *fmt, ...);
-	void PrintPhyAddr(uint8_t dat0, uint8_t dat1);
-	void PrintPacketNo( unsigned no );
-	void DumpHexData(uint8_t tel[], unsigned int start, unsigned int len);
-	void DumpMemData(uint8_t tel[], unsigned int len, unsigned ExtendedOffset, unsigned ItsUserMem = 0);
-	void DumpPropValHeader(uint8_t tel[], unsigned int ExDReq);
-	void DumpLoadAddressTel(uint8_t tel[], unsigned int len);
-	void DbgParseTele(uint8_t tel[], unsigned int len);
-	virtual void OutputFunction(char *line, unsigned int len) = 0;
+    unsigned linelen;
+    unsigned remlen;
+    char* linebuffer;
+    void buf_printf(const char *fmt, ...);
+    void PrintPhyAddr(uint8_t dat0, uint8_t dat1);
+    void PrintPacketNo( unsigned no );
+    void DumpHexData(uint8_t tel[], unsigned int start, unsigned int len);
+    void DumpMemData(uint8_t tel[], unsigned int len, unsigned ExtendedOffset, unsigned ItsUserMem = 0);
+    void DumpPropValHeader(uint8_t tel[], unsigned int ExDReq);
+    void DumpLoadAddressTel(uint8_t tel[], unsigned int len);
+    void DbgParseTele(uint8_t tel[], unsigned int len);
+    virtual void OutputFunction(char *line, unsigned int len) = 0;
 };
 
 #endif /* TEL_DUMP_H_ */
