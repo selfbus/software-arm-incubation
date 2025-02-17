b3e03d8332e81cb22c2de67c9efdd472a7d6ec78
Darthyson <darth@maptrack.de>
2025-02-17 19:58:52 +0100

    [ft12] Move configuration to new `config.h`

--- misc/ft12/src/app_main.cpp
+++ misc/ft12/src/app_main.cpp
@@ -16,25 +16,10 @@
 #include <sblib/bits.h>
 #include <sblib/version.h>
 #include <cstring>
+#include "config.h"
 #include "bcu_ft12.h"
 #include "ft12_protocol.h"
 
-#define LED_SERIAL_RX               (PIO0_6) //!< Serial-Rx LED Pin
-#define LED_KNX_RX                  (PIO0_7) //!< KNX-Rx LED Pin
-#define LED_ON                      (false)  //!< Led is turned on by setting the IO-pin to low/ground
-#define LED_OFF                     (true)   //!< Led is turned off by setting the IO-pin to high/+3V3
-#define LED_TEST_MS                 (250)    //!< Time in milliseconds every led should light up on startup test
-
-#define PIN_FT_SERIAL_TX            (PIN_TX) //!< Serial-Tx Pin
-#define PIN_FT_SERIAL_RX            (PIN_RX) //!< Serial-Rx Pin
-
-#define LED_KNX_RX_BLINKTIME        (100)    //!< Receiving KNX packets blinking timeout in milliseconds
-
-#define FT_OWN_KNX_ADDRESS          (0x11fe) //!< Our own knx-address: 1.1.254
-#define FT_FRAME_SIZE               (32)     //!< Maximum size of FT1.2 frames
-#define FT_MAX_SEND_RETRY           (0)      //!< Do not repeat sending
-#define FT_BAUDRATE                 (19200)  //!< Ft12 baudrate
-#define FT_OUTBUFFER_COUNT          (2)      //!< Number of outgoing FT12 frame buffers
 APP_VERSION("SBft12  ", "0", "02")  // Don't forget to also change the build-variable sw_version
 
 BcuFt12 bcu = BcuFt12();  //!< Bus coupling unit Maskversion 0x0012 of the ft12 module
