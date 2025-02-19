714c2b3476920e27c1414019ec35b464dc6e6d86
Darthyson <darth@maptrack.de>
2025-02-19 17:22:43 +0100

    [ft12] Wait for ft ack (timeout) in `processTelegram()`

--- misc/ft12/src/app_main.cpp
+++ misc/ft12/src/app_main.cpp
@@ -475,6 +475,8 @@ bool processVariableFrame(uint8_t* frame, uint8_t length)
  */
 void processTelegram()
 {
+    while (ackPending())
+         ;
     memcpy(ftFrameOut + VARIABLE_FRAME_HEADER_LENGTH, bcu.bus->telegram, bcu.bus->telegramLen - 1);
     sendVariableFrame(ftFrameOut, FC_SEND_UDAT, L_Data_Ind, bcu.bus->telegramLen + 1);
 }
